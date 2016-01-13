5f917a030f276af197b9b507d856f2485008bb9b
Paul Beck <pb@teamgeist-medien.de>
2016-01-13 14:11:30 +0100

    Initial

---
+++ ext_emconf.php
@@ -0,0 +1,34 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext: "tgm_seo"
+ *
+ * Generated 2015-07-30
+ *
+ * Manual updates:
+ * Only the data in the array - anything else is removed by next write.
+ * "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'TYPO3 SEO improvement',
+	'description' => 'Erweitert die Seiteneigenschaften um MetaTags wie robots',
+	'category' => 'plugin',
+	'author' => 'Steffen Thierock',
+	'author_email' => 'st@teamgeist-medien.de',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => '0',
+	'createDirs' => '',
+	'clearCacheOnLoad' => 0,
+	'version' => '',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '6.2',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+);
---
+++ ext_tables.sql
@@ -0,0 +1,7 @@
+#
+# Table structure for table 'pages'
+#
+CREATE TABLE pages (
+	meta_robots int(11) DEFAULT '0' NOT NULL,
+	canonical varchar(255) DEFAULT '' NOT NULL,
+);
