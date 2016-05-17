babc039fcd5fc115ad5915bfcb5f47b356824d80
Oliver Pfaff <op@teamgeist-medien.de>
2016-05-17 14:42:36 +0200

    [TASK] Kompat. 7.6
    TCA Page nach TCA/Overrides/pages verschoben

---
+++ Configuration/TCA/Overrides/pages.php
@@ -0,0 +1,33 @@
+<?php
+$tmp_cr_seitenattribute_columns = array(
+	'meta_robots' => array(
+		'exclude' => 1,
+		'label' => 'Robots',
+		'config' => array(
+			'type' => 'select',
+			'items' => array(
+                array('index, follow', 0),
+                array('index, nofollow', 1),
+                array('noindex, follow', 2),
+                array('noindex, nofollow', 3),
+            ),
+			'size' => 1,
+			'maxitems' => 1,
+			'eval' => ''
+		),
+	),
+	'canonical' => array (
+		'exclude' => 1,
+		'label' => 'Canonical',
+		'config' => array (
+				'type' => 'input',
+				'size' => '45',
+				'max' => '255',
+				'eval' => 'trim',
+		)
+	),
+);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('pages', $tmp_cr_seitenattribute_columns, 1);
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addFieldsToPalette('pages', 'metatags', '--linebreak--,meta_robots,--linebreak--,canonical', 'after:description');
+
